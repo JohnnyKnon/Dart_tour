92fba7442cfd7e4d6990a0d830afb728d8114211
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-05 12:18:25 +0900

    Dart:: dynamic 키워드변수

--- main.dart
+++ main.dart
@@ -1,7 +1,14 @@
 void main() {
-  String myName = "성민홍"; // String type
-  bool isMale = true; // boolean type
-  int age = 19; // int type - 정수
-  double weight = 55.5; // double type - 실수
-  num anyNumber = 19.5; // num type - 실수,정수 전부 가능 하지만 숫자값
+  // 이와 같이 var 키워드 변수에 디폴트값이 없을 경우에는 Dynamic 타입이됨
+  var name;
+  name = "Hello"; // 문자열도 가능
+  name = 1; // 숫자도 가능
+  name = true; // 불리언도 가능
+  print(name); // true 가 호출됨
+
+  // dynamic 키워드로 변수선언 시 어떤 타입이던지 넣어줄 수 있음
+  dynamic anything = 'Hello';
+  anything = 1;
+  anything = false;
+  print(anything);
 }
