fc7fb2b5bb5aca6313b71adb5171c81f1f5c99a6
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-05 18:09:48 +0900

    Dart:: Null 여부 체크하기 단축표현

--- main.dart
+++ main.dart
@@ -1,9 +1,5 @@
 void main() {
-  String johnny = '성민';
-  johnny = null; // String 타입이기 때문에 null 이 들어갈 수 없음
-
   String? box = "Apple"; // type 뒤에 ?(물음표가 붙게되면) null을 허용하게 된다.
   box = null;
-
-  print(box);
+  print(box?.isNotEmpty);
 }
