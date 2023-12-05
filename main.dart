cf9a89ad68002c3f501970238fd7792370ddc3b3
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-05 11:34:54 +0900

    Dart:: 변수 var 키워드

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
 void main() {
-  print('Hello world');
+  // 첫번째 변수선언 키워드 var -> 변수의 타입을 구체화 할 필요는 없다. 그 이유는 Dart는 처음 선얼할때의 Data Type을 그대로 가져가기 때문
+  var myName = "성민홍";
+  // myName = 1; 와 같이 작성하게되면 int 값이기 때문에 String 변수에 넣어줄 수 없다는 에러호출
 }
