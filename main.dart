646cd1b430223278fda8355e8ece366edd3dcc58
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-05 11:44:09 +0900

    Dart:: 변수 데이터 타입 명시한 변수

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 void main() {
-  // 첫번째 변수선언 키워드 var -> 변수의 타입을 구체화 할 필요는 없다. 그 이유는 Dart는 처음 선얼할때의 Data Type을 그대로 가져가기 때문
-  var myName = "성민홍";
-  // myName = 1; 와 같이 작성하게되면 int 값이기 때문에 String 변수에 넣어줄 수 없다는 에러호출
+  String myName = "성민홍"; // String type
+  bool isMale = true; // boolean type
+  int age = 19; // int type - 정수
+  double weight = 55.5; // double type - 실수
+  num anyNumber = 19.5; // num type - 실수,정수 전부 가능 하지만 숫자값
 }
