7807d33c031a2e68b3f1b2752e97fb4042416323
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-07 21:36:32 +0900

    Dart:: late 키워드 연습

--- main.dart
+++ main.dart
@@ -1,13 +1,17 @@
 void main() {
-  // late 키워드 (final 이나 var 앞에 붙여줄 수 있다. )
-  // late는 초기 데이터 없이 변수를 선언할 수 있게 해줌
+  // late 키워드 연습
   late final String name;
-  name = '성민';
+  late final int age;
+  late final bool isMale;
+  late final String id;
 
-  // 이렇게 작업을 하게 되면 실수를 방지 시켜줌 아래와 같은 코드가 있을 경우
-  // 기존 var 로 print 하면 null 이 호출되는 반면
-  // late 가 존재하는 var의 경우에는 print(age) 처럼 사용하게되면 에러를 호출함
-  late var age;
+  name = '홍성민';
+  age = 19;
+  isMale = true;
+  id = 'user0001';
 
-  print(age); // 오류가 남 null safety 같은 역할을 함
+  print(name);
+  print(age);
+  print(isMale);
+  print(id);
 }
