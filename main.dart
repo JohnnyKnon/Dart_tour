c482e8b34351d8c17a8c3384a9fc0100d62291d8
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-11 21:26:57 +0900

    Dart:: List의collection for 기능 살펴보기

--- main.dart
+++ main.dart
@@ -1,11 +1,13 @@
 void main() {
-  // collection for 전에 String interpolation 알아보자
-  var name = "Johnny";
-  var age = 10;
-  // 방법은 간단하다 단순하게 변수만 담고 싶다면 문자열을 생성해주고 문자열 내부에 $변수명을 넣게 되면 사용가능
-  // 하지만 무언가를 계산해야된다면 ${변수와 계산식}을 넣게 되면 사용가능
-  var greeting =
-      "Hello everyone, my name is $name and I'm ${age + 9} years old ";
+  // Collection for 대해서 소개
+  // 아래와 같이 for문을 내부에서 사용해서 추가할 수 있다.
+  // 하단 for in 은 JS와 동일한 문법형태를 갖추고있음
+  List<Object> oldFriends = ['mana', 'hyunjin', 'minjun'];
+  List<Object> newFriends = [
+    'jimin',
+    'minhyung',
+    for (var friend in oldFriends) "Luv ${friend}"
+  ];
 
-  print(greeting);
+  print(newFriends);
 }
