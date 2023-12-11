476e3f9b69299aa17cf733d90b6ea9a3a717113f
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-11 21:10:38 +0900

    Dart:: List 타입과 collection if

--- main.dart
+++ main.dart
@@ -1,11 +1,31 @@
 void main() {
-  // 'const 키워드 (상수 BUT JS에서 나오는 const 키워드의 상수와는 다르다)'
-  // dart의 const는 compile-time constant를 만들어줌
-  const API =
-      fetchApi(); // 이렇게 작업할 경우 compile-time 이 아닌 Runtime에서 확인 해야되기 때문에 오류남
+  // List
+  // 아래 numbers 를 가리키면 List<int> 라는 데이터 타입을 볼 수 있다. 여러 데이터 타입이 섞이면 <Object> 로 바뀐다.
+  var numbers = [1, 2, 3, 4];
 
-  // 따라서 앱을 배포할때 들어가는 환경상수등을 선언할때 사용
-  const max_allowed_price = 120;
-  const min_allowed_price = 10;
-  const max_user_name = 15;
+  // 위와 같이 우리는 List<int> 를 타입으로 변수설정 가능 <int> 이기 때문에 내부에 add('string') 시 오류남
+  List<int> numbersTwo = [5, 6, 7, 8];
+  // numberTwo.add('string'); -> Error
+
+  // 하지만 위에 여러 데이터 타입이 섞이면 <Object> 가 된다라고 쓰여져 있는 것 처럼 문자열 추가도 됨
+  List<Object> numbersThree = [1, 2, 3, 4];
+  numbersThree.add('TEST');
+
+  print(numbersThree.first); // 첫번째 호출
+  print(numbersThree.last); // 마지막 호출
+
+  // Dart에서 List의 최대의 장점
+  // -> collection if 와 collection for을지원함 (지원하는 언어 많이없음)
+
+  // Firstly : Collection if
+  var giveMeFive = true;
+  List<Object> numbersFour = [
+    10,
+    11,
+    12,
+    13,
+    if (giveMeFive) 5, // giveMeFive가 트루이면 5를 넣어준다 라는 의미
+  ];
+
+  print(numbersFour);
 }
