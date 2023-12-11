2403169bb45871d1b9e6c112d6809f09083d66bc
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-11 21:42:13 +0900

    Dart:: Map 으로 JS의 Object 구현해보자

--- main.dart
+++ main.dart
@@ -1,13 +1,28 @@
 void main() {
-  // Collection for 대해서 소개
-  // 아래와 같이 for문을 내부에서 사용해서 추가할 수 있다.
-  // 하단 for in 은 JS와 동일한 문법형태를 갖추고있음
-  List<Object> oldFriends = ['mana', 'hyunjin', 'minjun'];
-  List<Object> newFriends = [
-    'jimin',
-    'minhyung',
-    for (var friend in oldFriends) "Luv ${friend}"
+  // Map -> JS의 객체같은 기능을함
+  // Map<key의 타입, value의 타입> 을 넣어서 설정해줄 수 있다.
+  // 따라서 <Object, Object> 를 할경우에는 어떤 데이터 타입도 들어올 수 있다. (dynamic 도 매한가지)
+  // 여기서 Object는 TS의 any 와 같이 Dart의 모든 타입은 객체이기에 Object는 어떤타입으로도 넣을 수 있음
+  Map<String, Object> player = {
+    'name': 'Johnny',
+    'job': 'gunner',
+    'level': 20,
+    'xp': 1.123,
+    'superpower': true
+  };
+
+  // 또한 복잡한 Map 도 구성가능
+  Map<List<int>, bool> testMap = {
+    [1, 2, 3]: true,
+    [4, 5, 6]: true,
+  };
+
+  // 또한 List 내부에 Map을 넣을 수 있다.
+  List<Map<String, Object>> user = [
+    {'name': 'Johnny', 'age': 19},
+    {'name': 'Elin', 'age': 16}
   ];
 
-  print(newFriends);
+  // 맵을 많이 사용하지 않는걸 추천하고 JS와 TS에서 사용하는 Object 형태로 사용하지 않는걸 추천한다함 (예: API 구조 데이터)
+  // Dart를 사용한다면 Map 이 아닌 class 로 구현하는걸 추천한다고함
 }
