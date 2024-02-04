2f8bdc6ea0952b64af322cf9c7071012a19e2ec4
sungmin Hong <johnyhong24@gmail.com>
2024-02-04 14:59:06 +0900

    Dart Abstract Class 공부

--- main.dart
+++ main.dart
@@ -1,3 +1,9 @@
+// abstract class 추상화 클래스
+// 추상화 클래스는 다른 클래스들이 가지고 있어야될 기반이되는 청사진 역할을 함
+abstract class Human {
+  void walk();
+}
+
 // Enum
 // 정해놓은 형태로 사용할 수 있음 Flutter에 Color.red 같은 느낌
 enum Team { red, blue }
@@ -5,17 +11,28 @@ enum Team { red, blue }
 enum XPLevel { begginer, medium, pro }
 
 // 대망의 class
-class Player {
+// extends를 통한 확장
+class Player extends Human {
   String name;
   XPLevel xp;
   Team team;
 
   Player({required this.name, required this.xp, required this.team});
 
+  void walk() {
+    print('Walking now');
+  }
+
   void sayHello() {
     var name = "여기는 내부 변수";
     print(
         'Hi my name is ${this.name}'); // this.name 도 되지만, class property와 variable이 겹치지 않는이상 this를 사용하지 않음
+  }
+}
+
+class Coach extends Human {
+  void walk() {
+    print('The coach is walking now');
   }
 }
 
