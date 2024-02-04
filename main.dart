18ca01aa708094de5dc8222c342b34a5551ed37f
sungmin Hong <johnyhong24@gmail.com>
2024-02-04 11:05:57 +0900

    Dart Set 문법

--- main.dart
+++ main.dart
@@ -1,28 +1,20 @@
 void main() {
-  // Map -> JS의 객체같은 기능을함
-  // Map<key의 타입, value의 타입> 을 넣어서 설정해줄 수 있다.
-  // 따라서 <Object, Object> 를 할경우에는 어떤 데이터 타입도 들어올 수 있다. (dynamic 도 매한가지)
-  // 여기서 Object는 TS의 any 와 같이 Dart의 모든 타입은 객체이기에 Object는 어떤타입으로도 넣을 수 있음
-  Map<String, Object> player = {
-    'name': 'Johnny',
-    'job': 'gunner',
-    'level': 20,
-    'xp': 1.123,
-    'superpower': true
-  };
+  // Set
+  var numbers = {1, 2, 3, 4};
+  // Set 타입 지정
+  Set<int> numbersTwo = {1, 2, 3, 4};
+  numbersTwo.add(1);
+  numbersTwo.add(1);
+  numbersTwo.add(5);
+  print(numbersTwo);
+  // {1,2,3,4, 5}
+  // 리스트일 경우에는 그대로 추가되겠지만, Set은 기본 구조는 List와 동일하지만, 모든 요소가 유니크해야됨
+  List<int> numbersThree = [1, 2, 3, 4];
+  numbersThree.add(1);
+  numbersThree.add(1);
+  numbersThree.add(1);
+  print(numbersThree);
+  // {1,2,3,4,1,1,1}
 
-  // 또한 복잡한 Map 도 구성가능
-  Map<List<int>, bool> testMap = {
-    [1, 2, 3]: true,
-    [4, 5, 6]: true,
-  };
-
-  // 또한 List 내부에 Map을 넣을 수 있다.
-  List<Map<String, Object>> user = [
-    {'name': 'Johnny', 'age': 19},
-    {'name': 'Elin', 'age': 16}
-  ];
-
-  // 맵을 많이 사용하지 않는걸 추천하고 JS와 TS에서 사용하는 Object 형태로 사용하지 않는걸 추천한다함 (예: API 구조 데이터)
-  // Dart를 사용한다면 Map 이 아닌 class 로 구현하는걸 추천한다고함
+  //요소가 중복ㄷ되지 않고 유니크한 값으로 유지하려면 Set 아니면 List
 }
