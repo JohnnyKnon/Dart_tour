bb8c1088be94f01dc7633f99f41979a08d024974
sungmin Hong <johnyhong24@gmail.com>
2024-02-04 12:30:57 +0900

    Dart typedef를 이용해서 alias 만들기

--- main.dart
+++ main.dart
@@ -1,29 +1,18 @@
-// QQ Operator
-String capitalizeName1(String? name) {
-  if (name != null) {
-    // if문 방식
-    return name.toUpperCase();
-  } else {
-    return "ANON";
-  }
+// Typedef -> 자료형이 헷갈릴 경우 사용하는 alias를 만드는 방법
+typedef ListOfInts = List<int>;
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
 }
 
-String capitalizeName2(String? name) =>
-    name != null ? name.toUpperCase() : "ANON"; // 삼항 연산자 방식
+typedef UserInfo = Map<String, String>;
 
-String capitalizeName3(String? name) =>
-    name?.toUpperCase() ??
-    'ANON'; // QQ방싣 여기서 name? 을 해준이유는 .toUpperCase 메소드 실행이 null 이면 안되기 때문에 null 이면 null -> ANON이 되는 형태
-// Left ?? Right 가 있다면 Left가 null 이면 Right가 실행되는 로직
+String sayHi(UserInfo userInfo) {
+  return "Hi ${userInfo['name']}";
+}
 
 void main() {
-  capitalizeName1('nico'); // NICO
-  capitalizeName2(null); // ANON
-  capitalizeName3('hong'); // HONG
-
-  // QQ equals or QQ assignment operator
-  String? nickname;
-  nickname ??=
-      'ANON'; // 먼저나온 QQ equals에서 이미 null 이 걸러지기 때문에 하단에 지정한 QQ equals는 작동X
-  nickname ??= 'another';
+  print(reverseListOfNumbers([1, 2, 3, 4]));
+  print(sayHi({"name": "mana"}));
 }
