5beb8957f6e5626ca0fe94d429a520aa4a2ed6b6
sungmin Hong <johnyhong24@gmail.com>
2024-02-04 13:03:15 +0900

    Dart Class Named Constructer Parameters 공부

--- main.dart
+++ main.dart
@@ -2,9 +2,15 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-// Costructor
-  Player(this.name, this.xp);
+// Costructor Named Constructor Parameters
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
 
   void sayHello() {
     var name = "여기는 내부 변수";
@@ -14,10 +20,17 @@ class Player {
 }
 
 void main() {
-  var player = Player("Johnny", 2000);
+  var player = Player(name: "Johnny", age: 20, team: 'Team A', xp: 1700);
   print(player.name);
   player.sayHello();
-  var player2 = Player("Manaka", 99999);
-  print(player2.name);
-  player2.sayHello();
+
+  var playerB = Player(name: "Jiyoon", age: 18, team: 'Team B', xp: 100);
+  print(playerB.name);
+  playerB.sayHello();
+
+  // Positional Constructor Parameters 사용 시 예시 -> 받아와야할 인수가 많아질 수 록 안좋음
+
+  // var player2 = Player("Manaka", 99999, 'Team A', 20);
+  // print(player2.name);
+  // player2.sayHello();
 }
