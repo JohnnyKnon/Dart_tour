82d614629877461175a03ecaed00e6184ec557ce
sungmin Hong <johnyhong24@gmail.com>
2024-02-04 13:38:14 +0900

    Dart Named Constructor 공부

--- main.dart
+++ main.dart
@@ -1,16 +1,31 @@
 // 대망의 class
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
-
 // Costructor Named Constructor Parameters
+// 이 형태는 기본 Constructor 임
   Player(
       {required this.name,
       required this.xp,
       required this.team,
       required this.age});
+
+  // Named Constructor
+  // : 콜론을 통해서 Player 객체를 초기화 함
+  // Named Constructor Parameters 에는 required 가 명시 되어있지 않기 때문에 필요함
+  Player.createBluePlayer({required String name, required int age})
+      : this.age = age,
+        this.name = name,
+        this.team = "blue",
+        this.xp = 0;
+
+  // Named Constructor Positional Syntex
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
 
   void sayHello() {
     var name = "여기는 내부 변수";
@@ -20,16 +35,16 @@ class Player {
 }
 
 void main() {
-  var player = Player(name: "Johnny", age: 20, team: 'Team A', xp: 1700);
+  // Named Constructor Parameters 사용
+  var player = Player.createBluePlayer(name: "Johnny", age: 20);
   print(player.name);
   player.sayHello();
 
-  var playerB = Player(name: "Jiyoon", age: 18, team: 'Team B', xp: 100);
+  var playerB = Player.createRedPlayer("Jiyoon", 18);
   print(playerB.name);
   playerB.sayHello();
 
-  // Positional Constructor Parameters 사용 시 예시 -> 받아와야할 인수가 많아질 수 록 안좋음
-
+  // Positional Constructor Parameters 사용전 예시 -> 받아와야할 인수가 많아질 수 록 안좋음
   // var player2 = Player("Manaka", 99999, 'Team A', 20);
   // print(player2.name);
   // player2.sayHello();
