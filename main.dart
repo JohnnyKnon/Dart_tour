41d8b409c95cd0c258aca2600c7db726a8c609d9
sungmin Hong <johnyhong24@gmail.com>
2024-02-04 15:26:13 +0900

    Dart Mixin 공부

--- main.dart
+++ main.dart
@@ -1,28 +1,35 @@
-class Human {
-  final String name;
-  Human({required this.name});
+// Mixin 공부하기
+// Mixin이란? Constructor(생성자)가 없는 클래스임
+mixin class Strong {
+  final double strengthLevel = 1500.99;
+}
 
-  void sayHello() {
-    print("Hi my name is $name");
+mixin QuickRunner {
+  void runQuick() {
+    print('RUUUUUUUUUN');
   }
+}
+
+mixin class Tall {
+  final double height = 1.99;
 }
 
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
-  Player({required this.team, required String name})
-      : super(name: name); // : super을 통해서 상속
-
-  // 여기서 지정하는 메소드로 대체
-  @override
-  void sayHello() {
-    super.sayHello();
-    print('and I play for ${team}');
-  }
+  Player({
+    required this.team,
+  });
 }
 
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
 void main() {
-  var player = Player(team: Team.blue, name: 'Johnny');
+  var player = Player(team: Team.blue);
+
+  player.runQuick();
 }
